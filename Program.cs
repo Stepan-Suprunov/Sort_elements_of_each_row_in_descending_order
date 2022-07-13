665ba68d09ea408763cdb3931e1f2748ba70b4d6
Stepan_Suprunov <ipsuprunovso@mail.ru>
2022-07-13 12:35:58 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// Отсортировать по убыванию элементы каждой строки двумерного массива
+
+
+void PrintArray(int[,] collection)
+{
+    for (int i = 0; i < collection.GetLength(0); i++)
+    {
+        for (int j = 0; j < collection.GetLength(1); j++)
+        {
+            Console.Write(collection[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("__________");
+}
+
+void FillArray(int[,] collection)
+{
+    for (int i = 0; i < collection.GetLength(0); i++)
+    {
+        for (int j = 0; j < collection.GetLength(1); j++)
+        {
+            collection[i,j] = new Random().Next(-99,100);
+        }
+    }
+}
+
+void Sorting(int[,] collection)
+{
+    for (int i = 0; i < collection.GetLength(0); i++)
+    {
+        for (int j = 0; j < collection.GetLength(1); j++)
+        {
+            int minIndex = j;
+            for (int k = j+1; k < collection.GetLength(1); k++)
+            {
+                if (collection[i,k] < collection[i,minIndex]) minIndex = k;
+            }
+            int mem = collection[i,j];
+            collection[i,j] = collection[i,minIndex];
+            collection[i,minIndex] = mem;
+        }
+    }
+}
+
+int[,] array = new int[3,4];
+
+FillArray(array);
+PrintArray(array);
+Sorting(array);
+PrintArray(array);
